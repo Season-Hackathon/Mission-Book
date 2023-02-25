34baf668fb4418206d9a43b2f0f6a0c9ebb181be
Yeji <mnbvcxzyj@gmail.com>
2023-02-25 14:59:55 +0900

    FE_PageMerge_fin

--- FE_MissionBook/js/mypage.js
+++ FE_MissionBook/js/mypage.js
@@ -18,17 +18,17 @@ function plus() {
   const total = document.getElementById("total");
   const result = cnt1 + cnt2 + cnt3;
 
-  console.log(cnt3);
   console.log(result);
   total.innerText = result;
 }
+
 
 // 색상 변경
 var beforeColor;
 
 window.onload = function () {
   init();
-  // changeTitle();
+  plus();
 }
 
 function init() {
@@ -60,15 +60,152 @@ function colorSet(target) {
 }
 
 // 칭호 변경 -> 하나로만 바뀌는 상태 
-function changeTitle() {
+function changeTitle1() {
+  // 바뀐 텍스트가 들어갈 곳
   var titleEl = document.getElementById("title");
-  var titleText = document.querySelector(".title-text").innerText;
-  console.log(titleText);
-  titleEl.innerText = titleText;
 
-  titleEl = ''
-  titleText = ''
+  // 바꿀 칭호 
+  var title1 = document.querySelector(".title-text1").innerText;
+
+  titleEl.innerText = title1;
 }
 
+function changeTitle2() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
 
+  // 바꿀 칭호 
+  var title2 = document.querySelector(".title-text2").innerText;
 
+  titleEl.innerText = title2;
+}
+
+function changeTitle3() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title3 = document.querySelector(".title-text3").innerText;
+
+  titleEl.innerText = title3;
+}
+
+function changeTitle4() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title4 = document.querySelector(".title-text4").innerText;
+
+  titleEl.innerText = title4;
+}
+
+function changeTitle5() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title5 = document.querySelector(".title-text5").innerText;
+
+  titleEl.innerText = title5;
+}
+
+function changeTitle6() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title6 = document.querySelector(".title-text6").innerText;
+
+  titleEl.innerText = title6;
+}
+
+function changeTitle7() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title7 = document.querySelector(".title-text7").innerText;
+
+  titleEl.innerText = title7;
+}
+
+function changeTitle8() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title8 = document.querySelector(".title-text8").innerText;
+
+  titleEl.innerText = title8;
+}
+
+function changeTitle8() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title8 = document.querySelector(".title-text8").innerText;
+
+  titleEl.innerText = title8;
+}
+
+function changeTitle9() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title9 = document.querySelector(".title-text9").innerText;
+
+  titleEl.innerText = title9;
+}
+
+function changeTitle10() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title10 = document.querySelector(".title-text10").innerText;
+
+  titleEl.innerText = title10;
+}
+
+function changeTitle11() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title11 = document.querySelector(".title-text11").innerText;
+
+  titleEl.innerText = title11;
+}
+
+function changeTitle12() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title12 = document.querySelector(".title-text12").innerText;
+
+  titleEl.innerText = title12;
+}
+
+function changeTitle13() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title13 = document.querySelector(".title-text13").innerText;
+
+  titleEl.innerText = title13;
+}
+
+function changeTitle14() {
+  // 바뀐 텍스트가 들어갈 곳
+  var titleEl = document.getElementById("title");
+
+  // 바꿀 칭호 
+  var title14 = document.querySelector(".title-text14").innerText;
+
+  titleEl.innerText = title14;
+}
